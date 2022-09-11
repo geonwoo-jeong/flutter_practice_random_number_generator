d6d731bafdf19057840a6830757d976905a42f1a
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2022-09-11 16:36:19 +0900

    add style

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:random_number_generator/constants/colors.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -11,32 +12,45 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: PRIMARY_COLOR,
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text('Random Number Generator'),
-                IconButton(onPressed: () {}, icon: Icon(Icons.settings))
-              ],
-            ),
-            Expanded(child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('123'),
-                Text('456'),
-                Text('789')
-              ],
-            )),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                  onPressed: (){},
-                  child: Text('Generate')),
-            )
-          ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Random Number Generator',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w700),
+                  ),
+                  IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.settings,
+                        color: RED_COLOR,
+                      ))
+                ],
+              ),
+              Expanded(
+                  child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [Text('123'), Text('456'), Text('789')],
+              )),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(primary: RED_COLOR),
+                    onPressed: () {},
+                    child: Text('Generate')),
+              )
+            ],
+          ),
         ),
       ),
     );
