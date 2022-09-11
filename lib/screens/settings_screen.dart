9cee9c4626b8826824cf18dc4925b928e3b89c15
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2022-09-11 17:29:40 +0900

    add settings screen using slider widget

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:random_number_generator/constants/colors.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({Key? key}) : super(key: key);
@@ -8,11 +11,48 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
+  double maxNumber = 10000;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Settings Screen'),
+      backgroundColor: PRIMARY_COLOR,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                child: Row(
+                  children: maxNumber
+                      .toInt()
+                      .toString()
+                      .split('')
+                      .map((e) => Image.asset(
+                            'assets/images/$e.png',
+                            width: 50.0,
+                            height: 70.0,
+                          ))
+                      .toList(),
+                ),
+              ),
+              Slider(
+                  value: maxNumber,
+                  min: 10000,
+                  max: 1000000,
+                  onChanged: (double val) {
+                    setState(() {
+                      maxNumber = val;
+                    });
+                  }),
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(primary: RED_COLOR),
+                  onPressed: () {},
+                  child: const Text('Save'))
+            ],
+          ),
+        ),
       ),
     );
   }
